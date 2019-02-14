21014fdf5c8ba55fe436bbe762b7f2cb17c71b18
Harrison Brock <harrison@harrisonbrock.com>
2019-02-14 16:21:59 -0500

    create project

---
+++ src/main/java/com/harrisonbrock/javajdp/JavaJdpApplication.java
@@ -0,0 +1,14 @@
+package com.harrisonbrock.javajdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJdpApplication.class, args);
+    }
+
+}
+
