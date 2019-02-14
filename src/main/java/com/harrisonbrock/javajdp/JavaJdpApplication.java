cc1efce3a04b08fd40b370f58dc9d092be157c4e
Harrison Brock <harrison@harrisonbrock.com>
2019-02-14 16:41:26 -0500

    added dependencies and setup queue
    Took 9 minutes

--- src/main/java/com/harrisonbrock/javajdp/JavaJdpApplication.java
+++ src/main/java/com/harrisonbrock/javajdp/JavaJdpApplication.java
@@ -1,14 +1,57 @@
 package com.harrisonbrock.javajdp;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaJdpApplication {
 
+
+    public static final String EXCHANGE = "gdp";
+    public static final String QUEUE_NAME = "Log";
+
     public static void main(String[] args) {
         SpringApplication.run(JavaJdpApplication.class, args);
     }
+
+    @Bean
+    public TopicExchange appExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Queue appQueue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding dealerBindLog() {
+        return BindingBuilder.bind(appQueue()).to(appExchange()).with(QUEUE_NAME);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter()
+    {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rt(final ConnectionFactory cf)
+    {
+        final RabbitTemplate rt = new RabbitTemplate(cf);
+        rt.setMessageConverter(producerJackson2MessageConverter());
+        return rt;
+    }
+
+
 
 }
 
